2893dd7d07c4305f695d02ddd43daa145a25b401
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-14 23:51:31 -0500

    ejercicio 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,7 +1,18 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
+		if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name
+
+		if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+		this._length = length;
+
+		if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = students;
   }
   
