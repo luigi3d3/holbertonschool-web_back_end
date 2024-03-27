701a667110be345566d13f298a182af04e110b1e
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-27 13:08:21 -0500

    ejercicio 4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,30 +1,40 @@
-// pricing.js
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
-class Pricing {
+export default class Pricing {
   constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
     this._amount = amount;
+
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency object');
+    }
     this._currency = currency;
   }
 
+  // Getter and setter for amount
   get amount() {
     return this._amount;
   }
 
   set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('amount must be a string');
+    }
     this._amount = newAmount;
   }
 
+  // Getter and setter for currency
   get currency() {
     return this._currency;
   }
 
   set currency(newCurrency) {
-    if (newCurrency instanceof Currency) {
-      this._currency = newCurrency;
-    } else {
-      throw new Error('Invalid currency object');
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('currency must be a number');
     }
+    this._currency = newCurrency;
   }
 
   displayFullPrice() {
@@ -35,5 +45,3 @@ class Pricing {
     return amount * conversionRate;
   }
 }
-
-export default Pricing;
