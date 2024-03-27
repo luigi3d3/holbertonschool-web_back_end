3c84b8305abf6cfd477467d6df782f9edb4785ce
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-27 10:28:43 -0500

    ejercicio 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Script that defines 'floor' function which takes a float
+and returns the floor of the float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float"""
+    return math.floor(n)
