b2e8df1f519f839a3b85622487363497c9cc5899
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-27 12:06:47 -0500

    ejercicio 7

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,21 +1,10 @@
-class Airport {
-    constructor(name, code) {
-      this._name = name;
-      this._code = code;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-    toString() {
-      return this._code;
-    }
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
   }
-  
-  export default Airport;
-  
+
+  toString() {
+    return `[${typeof this} ${this._code}]`;
+  }
+}
