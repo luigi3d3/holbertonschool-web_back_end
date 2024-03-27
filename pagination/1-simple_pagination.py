ec6492a99f5d1c0899b87eece085414e0ed11757
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-27 11:27:56 -0500

    Simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -3,9 +3,7 @@
 """Server class pagination"""
 
 import csv
-import math
 from typing import List, Tuple
-
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -26,29 +24,20 @@ class Server:
 
         return self.__dataset
 
-    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
-        """
-        Returns a tuple (start_index, end_index)
-        for a given page and page size
-        """
-        start_index = (page - 1) * page_size
-        end_index = start_index + page_size
-
-        return start_index, end_index
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Return the specified page of the dataset
-        based on the given page and page size
-        """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-
-        start_index, end_index = self.index_range(page, page_size)
+        assert isinstance(page, int) and page > 0, "Page must be an integer greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0"
+        
+        start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
 
         if start_index >= len(dataset):
             return []
-
+        
         return dataset[start_index:end_index]
-    
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns a tuple of two integers representing the start and end indices for the given page and page size."""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
