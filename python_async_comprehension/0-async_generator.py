20ea8f69ae19662b5a01f4ad3f293b84a5d3dd50
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-03-27 11:08:55 -0500

    ejercicio 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:
     """Coroutine that not takes arguments"""
     for _ in range(10):
         await asyncio.sleep(1)
