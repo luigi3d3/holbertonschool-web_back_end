8dc0e31e49c6c4e974690f79e16dcfef372d2cbe
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-04-28 22:54:05 -0500

    0. Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Script that define 'add' function that takes two float numbers
+and returns their sum
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers and returns the result"""
+    return a + b
