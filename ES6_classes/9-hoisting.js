3b2336e5833968dc9b9258bce9836b4f945005da
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-05-12 23:24:17 -0500

    update code

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,4 +1,5 @@
-class HolbertonClass {
+/* eslint-disable */
+export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -13,7 +14,10 @@ class HolbertonClass {
   }
 }
 
-class StudentHolberton {
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+
+export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
@@ -27,14 +31,7 @@ class StudentHolberton {
   get holbertonClass() {
     return this._holbertonClass;
   }
-
-  get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-  }
 }
-
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
@@ -44,5 +41,4 @@ const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 
-console.log(listOfStudents);
-console.log(listOfStudents.map(student => student.fullStudentDescription));
+export default listOfStudents;
