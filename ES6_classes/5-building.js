6ccddada8925ce1ec59507f7784ebdb85d9b49bc
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-05-12 22:53:38 -0500

    A Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+// building.js
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+export default Building;
