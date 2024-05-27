fb322b6b41397860f1ee05af3ca2f60d292fd992
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-05-26 19:14:47 -0500

    Catch me if you can!

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
+}
+
+export default handleResponseFromAPI;
