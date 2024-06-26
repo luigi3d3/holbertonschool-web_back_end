287453ac5e82eb40d7f581b4205e36d4bfff7e63
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-06-26 00:41:27 -0500

    update code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,29 +3,25 @@
 
 from pymongo import MongoClient
 
-
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
 
 def log_stats(mongo_collection, option=None):
     """provides stats about Nginx logs stored in MongoDB"""
     
-    items = {}
     if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
+        value = mongo_collection.count_documents({"method": {"$regex": option}})
         print(f"\tmethod {option}: {value}")
         return
 
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
+    total_logs = mongo_collection.count_documents({})
+    print(f"{total_logs} logs")
     print("Methods:")
     for method in METHODS:
-        log_stats(nginx_collection, method)
+        count = mongo_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
     status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
 
-
 if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+    mongo_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(mongo_collection)
